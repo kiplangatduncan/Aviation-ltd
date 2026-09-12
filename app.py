8085f9051da06ace978b637085c562753d27f206
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-12 15:02:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'change-this-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-  
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(200))
+    
